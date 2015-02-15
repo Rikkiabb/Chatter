39b91200f179f7699a23ec41ffdd3fc4b2f8184f
Haftor <haftor.g@.com>
2015-02-15 17:40:17 +0000

    views added

--- src/app.js
+++ src/app.js
@@ -4,29 +4,57 @@ ChatApp.config(
 	function($routeProvider){
 		$routeProvider
 		.when("/login", {templateUrl: "views/login.html", controller:"HomeController"})
+		.when('/rooms/:user/', { templateUrl: 'Views/rooms.html', controller: 'RoomsController' })
+		.when('/room/:user/:room/', { templateUrl: 'Views/room.html', controller: 'RoomController' })
 		.otherwise({redirectTo: "/login"});
 	}
 
 );
 
 
-ChatApp.controller('HomeController', function ($scope, socket) {
+ChatApp.controller('HomeController', function ($scope, $location, $rootScope, $routeParams, socket) {
 
 	$scope.nickname = '';
+	$scope.errorMessage = '';
 
 	$scope.login = function() {			
 		if ($scope.nickname === '') {
-			console.log("kjadbf");
+			$scope.errorMessage = 'Please choose a nick-name before continuing!';
 
 		} else {
 			socket.emit('adduser', $scope.nickname, function (available) {
 				if (available) {
-					console.log("added user");
+					$location.path('/rooms/' + $scope.nickname);
 				} else {
-					console.log("not added user");
+					$scope.errorMessage = 'This nick-name is already taken!';
 				}
 			});			
 		}
 
 	};
+});
+
+ChatApp.controller('RoomsController', function ($scope, $location, $rootScope, $routeParams, socket) {
+	// TODO: Query chat server for active rooms
+	$scope.rooms = ['Room 1','Room 2','Room 3','Room 4','Room 5'];
+	$scope.currentUser = $routeParams.user;
+});
+
+ChatApp.controller('RoomController', function ($scope, $location, $rootScope, $routeParams, socket) {
+	$scope.currentRoom = $routeParams.room;
+	$scope.currentUser = $routeParams.user;
+	$scope.currentUsers = [];
+	$scope.errorMessage = ''
+
+	socket.on('updateusers', function (roomName, users, ops) {
+		// TODO: Check if the roomName equals the current room !
+		$scope.currentUsers = users;
+	});		
+
+	socket.emit('joinroom', $scope.currentRoom, function (success, reason) {
+		if (!success)
+		{
+			$scope.errorMessage = reason;
+		}
+	});
 });
