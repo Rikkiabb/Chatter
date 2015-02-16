bc69889056dbc17dba25701dc14b83e8351c62d4
Haftor <haftor.g@.com>
2015-02-16 20:40:03 +0000

    sendmessage working

--- src/roomController.js
+++ src/roomController.js
@@ -2,13 +2,39 @@ ChatApp.controller('RoomController', function ($scope, $location, $rootScope, $r
 	$scope.currentRoom = $routeParams.room;
 	$scope.currentUser = $routeParams.user;
 	$scope.currentUsers = [];
-	$scope.errorMessage = ''
+	$scope.errorMessage = '';
+	$scope.messages = [];
+	$scope.message = '';
+	var objMessage = {
+		roomName : "lobby",
+		msg : $scope.message
+		
+	};
+	$scope.sendMessage = function() {
+		console.log($scope.message, "<-------");
+		if($scope.message === ''){
+			console.log("nothing");
+		}
+		else{ 
+			objMessage.msg = $scope.message;
+			//console.log(objMessage, "----");
+			//console.log($routeParams.room, "---");
+			socket.emit('sendmsg', objMessage);
+		}
+	};
 
-	socket.on('updateusers', function (roomName, users, ops) {
-		// TODO: Check if the roomName equals the current room !
+	socket.on('updatechat', function (roomName, msgHistory){
+		$scope.roomName = roomName;
+		$scope.msg = msgHistory;
+		console.log("updatechat!!!!");
+	});
+	socket.on('cons', function (roomName, msgHistory){
+		console.log("<--------cool");		
+	})
+
+	socket.on('updateusers', function (roomName, users, ops) {	
 		$scope.currentUsers = users;
 	});		
-
 	socket.emit('joinroom', $scope.currentRoom, function (success, reason) {
 		if (!success)
 		{
