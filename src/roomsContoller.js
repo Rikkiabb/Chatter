24e92f2f8689fc5652b4f6f53a29815947fefe85
Stefán <stefanh13@ru.is>
2015-02-16 21:05:29 +0000

    Allow the user to add and go to a chat room

--- src/roomsContoller.js
+++ src/roomsContoller.js
@@ -1,5 +1,54 @@
 ChatApp.controller('RoomsController', function ($scope, $location, $rootScope, $routeParams, socket) {
 	// TODO: Query chat server for active rooms
-	$scope.rooms = ['Room 1','Room 2','Room 3','Room 4','Room 5'];
+	$scope.rooms = {};
 	$scope.currentUser = $routeParams.user;
+	$scope.showInput = false;
+	
+
+
+
+	socket.on('roomlist', function(list){
+				
+				$scope.rooms = Object.keys(list);
+				
+	});
+
+	
+	socket.emit('rooms');
+	// socket.on('cons', function(breyta){console.log("cons");console.log(breyta);});
+	// socket.on('con', function(breyta){console.log("con");console.log(breyta);});
+
+	$scope.createRoom = function(){
+		var obj = {
+			room: $scope.roomName,
+			pass: undefined
+		};
+		socket.emit('joinroom', obj  ,function (success, reason) {
+			if (!success){
+				$scope.errorMessage = "WELLWELLWELL";
+			}
+			else{
+				socket.emit('rooms');
+			}
+		});
+		
+	};
+
+
+
+
+	// socket.on('updateusers', function(a, b, c){
+	// 		console.log(a);
+
+
+	// });
+
+
+	if($scope.showInput === false){
+		$scope.buttonName = "Create Room";
+	}
+	else
+	{
+		$scope.buttonName = "Close";
+	}
 });
